65aa133777c6534cc4a9337e1cbb169b3dac1656
서인교 <saingo4202@gmail.com>
2024-08-12 06:08:11 +0900

    1. flyway 수정

--- src/main/resources/db/migration/V4.0__create_posts_table.sql
+++ src/main/resources/db/migration/V4.0__create_posts_table.sql
@@ -1,11 +1,18 @@
 CREATE TABLE `posts` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
   `content` text DEFAULT NULL,
-  `last_modified_user` varchar(100) NOT NULL,
+  `created_time` datetime(6) NOT NULL,
+  `last_modified_time` datetime(6) NOT NULL,
   `title` varchar(255) DEFAULT NULL,
   `visible` enum('PUBLISHED','UNPUBLISHED') NOT NULL,
   `category_id` bigint(20) NOT NULL,
+  `created_user_id` bigint(20) NOT NULL,
+  `last_modified_user_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FKijnwr3brs8vaosl80jg9rp7uc` (`category_id`),
-  CONSTRAINT `FKijnwr3brs8vaosl80jg9rp7uc` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `FKld60dvpudryik7uvssnjjbjjf` (`created_user_id`),
+  KEY `FKsx3u648k9vhhfqd1trx9109xr` (`last_modified_user_id`),
+  CONSTRAINT `FKijnwr3brs8vaosl80jg9rp7uc` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  CONSTRAINT `FKld60dvpudryik7uvssnjjbjjf` FOREIGN KEY (`created_user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FKsx3u648k9vhhfqd1trx9109xr` FOREIGN KEY (`last_modified_user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- src/main/resources/db/migration/V5.0__create_comments_table.sql
+++ src/main/resources/db/migration/V5.0__create_comments_table.sql
@@ -1,10 +1,17 @@
 CREATE TABLE `comments` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `content` varchar(255) NOT NULL,
-  `last_modified_user` varchar(100) NOT NULL,
+  `created_time` datetime(6) NOT NULL,
+  `last_modified_time` datetime(6) NOT NULL,
   `visible` enum('PUBLISHED','UNPUBLISHED') NOT NULL,
+  `created_user_id` bigint(20) NOT NULL,
+  `last_modified_user_id` bigint(20) NOT NULL,
   `post_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
+  KEY `FK3penry40606gtq1g98nbxvhvc` (`created_user_id`),
+  KEY `FK45yiym4v7w38sg55ggk4x9fs4` (`last_modified_user_id`),
   KEY `FKh4c7lvsc298whoyd4w9ta25cr` (`post_id`),
+  CONSTRAINT `FK3penry40606gtq1g98nbxvhvc` FOREIGN KEY (`created_user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FK45yiym4v7w38sg55ggk4x9fs4` FOREIGN KEY (`last_modified_user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `FKh4c7lvsc298whoyd4w9ta25cr` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
