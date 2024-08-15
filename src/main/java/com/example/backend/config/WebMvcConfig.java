63bce67c2015bb0e1d2a46b7d1c044175f44b139
서인교 <saingo4202@gmail.com>
2024-08-16 03:23:33 +0900

    1. cors 수정

--- src/main/java/com/example/backend/config/WebMvcConfig.java
+++ src/main/java/com/example/backend/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.example.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:///C:/projects/file-system/");
+    }
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://blog.rocd.site")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
     
 }
